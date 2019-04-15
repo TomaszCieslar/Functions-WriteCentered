62af48583e69f643c068bd4e93c3d1ba8918b25b
Tomasz Cieslar <tomasz.cieslar@capgemini.com>
2019-04-15 04:48:48 +0200

    final solution

--- Functions-WriteCentered/Program.cs
+++ Functions-WriteCentered/Program.cs
@@ -4,9 +4,32 @@ namespace Functions_WriteCentered
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Create a function to write centered on screen the text that is 
+            //indicated as a parameter(supposing a screen width of 80 characters): 
+
+            //WriteCentered("Hello!");
+
+            WriteCentered("Hello!");
+            Console.ReadLine();
+        }
+
+        private static void WriteCentered(string text)
+        {
+            int i = 0;
+
+            for (; i < 17; i++)
+                Console.WriteLine();
+
+            for (i = 0; i < 36; i++)
+                Console.Write(" ");
+
+            Console.Write(text);
+
+            for (i = 0; i < 14; i++)
+                Console.WriteLine();
         }
     }
 }
